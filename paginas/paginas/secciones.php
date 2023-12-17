61f9418c02b0928ffa76cd045142a80a68f2dc9d
raynerf1 <101900356+raynerf1@users.noreply.github.com>
2023-12-17 02:58:48 +0100

    Actualización secciones.php

--- paginas/paginas/secciones.php
+++ paginas/paginas/secciones.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <!--Index-->
 
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Grocesort by Rayner</title>
-      <meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>SportsStickerSpotter - Equipos</title>
+        <meta name="description" content="Grocesort, aplicación web para comparar los precios de los productos en varios supermercados de España">
+    <meta property="og:title" content="Grocesort">
+    <meta property="og:description" content="Grocesort, aplicación web para comparar los precios de los productos en varios supermercados de España">
+    <meta property="og:image" content="https://cdn.discordapp.com/attachments/1166363443637518346/1181710546878337146/GS_Logo.png?ex=65820cb5&is=656f97b5&hm=ed861c8c0bc3f030314a66d50da7e1cd14a9f4f97917927f63252dc64379481f&">
+    <meta property="og:image:alt" content="Logo de Grocesort">
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/styles.min.css">
     <link rel="stylesheet" href="../assets/css/preloader.css">
@@ -50,16 +54,29 @@
         <a class="nav-link" aria-current="page" href="../../index.html">Inicio</a>
         <a class="nav-link" href="sobremi/index.html">Sobre mí</a>
         <a class="nav-link" href="privacidad.html">Privacidad</a>
-        
+        <!--Theme mode switcher-->
+        <!--
+        <a class="nav-link" id="cambiarEstilos" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Cambiar modo de color de la página" style="cursor: pointer; <?php if ($_COOKIE["FormularioCookies"] === 'No'){echo 'display: none';}  ?>" >
+    <span id="modoTexto">Claro</span>
+    <svg xmlns="http://www.w3.org/2000/svg"  width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16" id="modoIcono">
+        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"></path>
+    </svg>
+</a>-->
+
+
       </div>
+
                 <?php 
                 $carro = isset($_COOKIE["carro"]) ? json_decode($_COOKIE["carro"], true) : [];
                 $cantidadElementos = count($carro);?>
                 <a class="nav-link" href="carro.php">
-    <button type="button" class="btn btn-primary ml-5" <?php if ($_COOKIE["FormularioCookies"] === 'No'){echo 'style="display: none"';}  ?>>
-        Cesta<span class="badge text-bg-secondary"><?php echo $cantidadElementos;?></span>
-    </button>
-</a>
+                    <button type="button" class="btn btn-primary ml-5" <?php if ($_COOKIE["FormularioCookies"] === 'No'){echo 'style="display: none"';}  ?>>
+                        Lista<span class="badge text-bg-secondary"><?php echo $cantidadElementos;?></span>
+                    </button>
+                </a>
+                
+
+                
               </div>
   </div>
 </nav>
@@ -85,7 +102,7 @@
 </div>
     </div>
 
-    <div id="div1" class="CentrarTest1">
+    <div id="div1" class="CentrarTest1 bg-white">
         <div class="spinner-border text-warning" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
@@ -93,7 +110,7 @@
 <nav class="mt-1">
 <br>
 </nav>
-      <div id="div2">
+      <div id="div2" class="bg-white">
 
 
 <nav class="mt-5">
@@ -143,10 +160,13 @@
 
             function rechazarCookies() {
                 document.cookie = 'carro' + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/paginas/paginas';
+                document.cookie = 'TemaModo' + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
                 document.cookie = "FormularioCookies=No; expires=<?php echo date('D, d M Y H:i:s', $tiempoExpiracion) . ' GMT'; ?>; path=/";
                     var enlaceCarro = document.querySelector('.nav-link[href="carro.php"]');
+                    var TemaModobtn = document.querySelector('#cambiarEstilos');
                 if (enlaceCarro) {
                     enlaceCarro.remove();
+                    TemaModobtn.remove();
                 }
             }
             </script>
@@ -183,15 +203,15 @@
     <input type="hidden" name="foto_slider" value="test<?php echo $fila["nombre"]; ?>">
     <input type="hidden" name="foto_logo" value="test<?php echo $fila["nombre"]; ?>">
     <a class="justify-content-center d-flex align-items-center columna" href="#" onclick="event.preventDefault(); this.closest('form').submit();">
-      <div class="card" style="border-style: none;">
+      <div class="card bg-white textoOscuro" style="border-style: none;">
         <div class="card-body">
           <h1 class="text-start"><?php echo ($fila["nombreutf"] !== null) ? $fila["nombreutf"] : $fila["nombre"]; ?></h1>
 
-          <img class="img-fluid order-4 coche" src=<?php echo $fila["imagen"]; ?> width="700px">
+          <img class="img-fluid order-4 coche" src="<?php echo $fila["imagen"]; ?>" width="700px" alt="Imagen de <?php echo ($fila["nombreutf"] !== null) ? $fila["nombreutf"] : $fila["nombre"]; ?>">
                                               
         </div>
       </div>
-      <img class="logo" src=<?php echo '../imagenes/secciones/iconos/' . str_replace(' ', '', strtolower(trim($fila["nombre"]))) . '.png' ?> width="100px" style="position: absolute;">
+      <img class="logo" src=<?php echo '../imagenes/secciones/iconos/' . str_replace(' ', '', strtolower(trim($fila["nombre"]))) . '.png' ?> width="7%" style="position: absolute;">
     </a>
   </form>
 </div>
@@ -212,6 +232,7 @@
     </section>
     <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
   <script src="../assets/js/script.min.js"></script>
+
   <script>
     gsap.from('section', {opacity: 0, duration: 4, y:50})
     gsap.from('.text-start', {opacity: 0, duration: 5, y:-70})
@@ -219,6 +240,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function() {
+        aplicarEstilos();
 
         setTimeout(function() {
             $('#div1').hide();
@@ -258,14 +280,83 @@
             }
         });
     });
+
+function getCookie(nombre) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${nombre}=`);
+    
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+    }
+    
+    return null;
+}
+
+// Obtén el valor de la cookie
+var modo = getCookie('TemaModo');
+var FCookies = getCookie('FormularioCookies');
+
+// Si la cookie está vacía, establece el valor predeterminado y actualiza la cookie
+if (!modo && FCookies === "Si") {
+    modo = "claro";
+    document.cookie = "TemaModo=claro; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}
+
+// Función para aplicar estilos según el modo
+function aplicarEstilos() {
+    if (modo === "oscuro") {
+        $('.bg-white').removeClass('bg-white').addClass('bg-dark');
+        $('.textoOscuro').removeClass('textoOscuro').addClass('text-white');
+        $('#modoTexto').text('Oscuro');
+        $('#modoIcono').show(); // Muestra el icono
+        modo = "oscuro";
+    } else {
+        $('.bg-dark').removeClass('bg-dark').addClass('bg-white');
+        $('.text-white').removeClass('text-white').addClass('textoOscuro');
+        $('#modoTexto').text('Claro');
+        $('#modoIcono').show(); // Muestra el icono
+        modo = "claro";
+    }
+}
+
+// Llama a la función para aplicar estilos al cargar la página
+
+
+// Función para cambiar los estilos y actualizar la cookie
+function cambiarEstilos() {
+    var textoModoBoton = document.getElementById("cambiarEstilos");
+
+    if (modo == "claro") {        
+        $('.bg-white').removeClass('bg-white').addClass('bg-dark');
+        $('.textoOscuro').removeClass('textoOscuro').addClass('text-white');
+        textoModoBoton.firstChild.nodeValue = '';
+        modo = "oscuro";
+    } else {
+        $('.bg-dark').removeClass('bg-dark').addClass('bg-white');
+        $('.text-white').removeClass('text-white').addClass('textoOscuro');
+        textoModoBoton.firstChild.nodeValue = "";
+        modo = "claro";
+    }
+
+    // Actualiza el valor de la cookie con el nuevo modo
+    document.cookie = "TemaModo=" + modo + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+    aplicarEstilos(); // Llama a la función para aplicar estilos después de cambiarlos
+}
+
+
+    $(document).ready(function () {
+        // Asigna la función al evento click del botón
+        $('#cambiarEstilos').click(cambiarEstilos);
+    });
+
 </script>
   </div>
   <div id="resultadoBusqueda">
   </div>
-    <footer id="footer" class="text-center bg-dark">
+    <footer id="footer" class="text-center bg-black">
         <div class="container text-white py-4 py-lg-5">
-        <li class="list-inline-item me-4"><a class="link-light" href="sobremi/index.html" target="_blank" content="no-referrer" rel="noreferrer">Sobre el desarrollador</a></li> <br>
-        <li id="androidTest" class="list-inline-item me-4"><a class="link-light" href="../../android/apk/grocesort.apk" content="no-referrer" rel="noreferrer" download>Descargar aplicación Android</a></li>
+            <ul><li class="list-inline-item me-4"><a class="link-light" href="sobremi/index.html" target="_blank" content="no-referrer" rel="noreferrer">Sobre el desarrollador</a></li></ul>
+            <ul><li id="androidTest" class="list-inline-item me-4"><a class="link-light" href="grocesort.apk" content="no-referrer" rel="noreferrer" download>Descargar aplicación Android</a></li></ul>
             <ul class="list-inline">
                 <li class="list-inline-item me-4"><a class="link-light" href="cookies.html">Política de Cookies</a></li>
                 <li class="list-inline-item"><a class="link-light" href="privacidad.html">Política de privacidad</a></li>
@@ -307,6 +398,10 @@
                 container.appendChild(elementosArray[i]);
             }
         }
+
     </script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+
 </body>
 </html>
